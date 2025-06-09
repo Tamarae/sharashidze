e9d55fe5aaf000e13b522c38111cf9ef034fd4f6
Tamara Kalkhitashvili <kalkhitashvili.tamar@gmail.com>
2025-06-10 00:18:45 +0400

    Add files via upload

---
+++ authority_data.js
@@ -0,0 +1,176 @@
+// authority_data.js
+// This file contains the biographical information for individuals,
+// generated from your TEI/XML prosopography file.
+
+const authorityData = {
+  "#p1001": {
+    name: "ბაგრატ IV",
+    patronymic: "ბაგრატიონი",
+    occupation: "გაერთიანებული საქართველოს მეფე, კურაპალატი",
+    bio: "საქართველოს მეფე 1027-1072 წლებში, გიორგი I-ის ძე...",
+    dates: "1018 - 1072-11-24",
+    relatedPersons: ["#p1001_a", "#p1001_b", "#p1001_c", "#p1001_d", "#p1002", "#p1001_f"]
+  },
+  "#p1002": {
+    name: "გიორგი II",
+    patronymic: "ბაგრატიონი",
+    occupation: "საქართველოს მეფე",
+    dates: "c. 1050 - c. 1112",
+    relatedPersons: ["#p1001", "#p1001_d", "#p1003"]
+  },
+  "#p1003": {
+    name: "დავით IV აღმაშენებელი",
+    patronymic: "ბაგრატიონი",
+    occupation: "საქართველოს მეფე",
+    dates: "1073 - 1125-01-24",
+    relatedPersons: ["#p1002", "#p1004"]
+  },
+  "#p1004": {
+    name: "დემეტრე I",
+    patronymic: "ბაგრატიონი",
+    occupation: "საქართველოს მეფე",
+    dates: "c. 1093 - 1156",
+    relatedPersons: ["#p1003", "#p1005"]
+  },
+  "#p1005": {
+    name: "გიორგი III",
+    patronymic: "ბაგრატიონი",
+    occupation: "საქართველოს მეფე",
+    dates: "unknown - 1184",
+    relatedPersons: ["#p1004", "#p1006"]
+  },
+  "#p1006": {
+    name: "თამარ მეფე",
+    patronymic: "ბაგრატიონი",
+    occupation: "საქართველოს მეფე",
+    dates: "c. 1160 - 1213-01-18",
+    relatedPersons: ["#p1005", "#p1007"]
+  },
+  "#p1007": {
+    name: "გიორგი IV ლაშა",
+    patronymic: "ბაგრატიონი",
+    occupation: "საქართველოს მეფე",
+    dates: "1192 - 1223-01-18",
+    relatedPersons: ["#p1006"]
+  },
+  "#p1008": {
+    name: "ყვარყვარე II ჯაყელი",
+    patronymic: "ჯაყელი",
+    occupation: "ათაბაგი",
+    dates: "c. 1416 - 1498-07-01",
+    relatedPersons: ["#p1009", "#p1010", "#p1011"]
+  },
+  "#p1009": {
+    name: "დედისიმედი",
+    patronymic: "",
+    occupation: "",
+    dates: "unknown - 1491-12-18",
+    relatedPersons: ["#p1008", "#p1010", "#p1011"]
+  },
+  "#p1010": {
+    name: "ქაიხოსრო I ჯაყელი",
+    patronymic: "ჯაყელი",
+    occupation: "ათაბაგი",
+    dates: "c. 1447 - 1500-05-06",
+    relatedPersons: ["#p1008", "#p1009", "#p1011"]
+  },
+  "#p1011": {
+    name: "მზეჭაბუკ I ათაბაგი",
+    patronymic: "ჯაყელი",
+    occupation: "ათაბაგი",
+    dates: "unknown - 1515",
+    relatedPersons: ["#p1008", "#p1009", "#p1010"]
+  },
+  "#p1012": {
+    name: "ყვარყვარე III ჯაყელი",
+    patronymic: "ჯაყელი",
+    occupation: "ათაბაგი",
+    dates: "c. 1469 - 1535",
+    relatedPersons: ["#p1010", "#p1013", "#p1015"]
+  },
+  "#p1013": {
+    name: "ანნა",
+    patronymic: "",
+    occupation: "",
+    dates: "",
+    relatedPersons: ["#p1012", "#p1015"]
+  },
+  "#p1014": {
+    name: "მზეჭაბუკ ჯაყელი",
+    patronymic: "ჯაყელი",
+    occupation: "",
+    bio: "Died young.",
+    dates: "",
+    relatedPersons: ["#p1012", "#p1013"]
+  },
+  "#p1015": {
+    name: "ქაიხოსრო II ჯაყელი",
+    patronymic: "ჯაყელი",
+    occupation: "ათაბაგი",
+    dates: "c. 1522 - 1573",
+    relatedPersons: ["#p1012", "#p1013"]
+  },
+  "#p1016": {
+    name: "სოილ",
+    patronymic: "კოპას ძე",
+    occupation: "ამილახორი, მხატვარი",
+    dates: "",
+    relatedPersons: ["#p1017", "#p1018", "#p1019"]
+  },
+  "#p1017": {
+    name: "რუამ-ყოფილი რაშთაელი",
+    patronymic: "კოპას ძე",
+    occupation: "",
+    dates: "",
+    relatedPersons: ["#p1016", "#p1018"]
+  },
+  "#p1018": {
+    name: "რაინდო-ყოფილი ეკატირინე",
+    patronymic: "",
+    occupation: "",
+    dates: "",
+    relatedPersons: ["#p1016", "#p1017"]
+  },
+  "#p1019": {
+    name: "მანგუშარ-ყოფილი მართა",
+    patronymic: "",
+    occupation: "",
+    dates: "",
+    relatedPersons: ["#p1016"]
+  },
+  "#p1001_a": {
+    name: "გიორგი I",
+    patronymic: "",
+    occupation: "",
+    dates: "",
+    relatedPersons: ["#p1001"]
+  },
+  "#p1001_b": {
+    name: "მარიამ არწრუნი",
+    patronymic: "",
+    occupation: "",
+    dates: "",
+    relatedPersons: ["#p1001"]
+  },
+  "#p1001_c": {
+    name: "ელენე არგიროსი",
+    patronymic: "",
+    occupation: "",
+    dates: "",
+    relatedPersons: ["#p1001"]
+  },
+  "#p1001_d": {
+    name: "ბორენა დედოფალი",
+    patronymic: "",
+    occupation: "",
+    dates: "",
+    relatedPersons: ["#p1001", "#p1002"]
+  },
+  "#p1001_f": {
+    name: "მარიამ-მართა",
+    patronymic: "",
+    occupation: "",
+    dates: "",
+    relatedPersons: ["#p1001"]
+  }
+};
---
+++ manuscript_data.js
@@ -0,0 +1,131 @@
+const teiData = {
+    "manuscripts": {
+        "Q-969": {
+            "idno": "Q-969 | კოპასძეთა ხელნაწერის ანდერძ–მინაწერები",
+            "repository": "ხელნაწერთა ეროვნული ცენტრი | National Centre of Manuscripts, Georgia",
+            "publication": "ქრ. შარაშიძე, სამხრეთ საქართველოს ისტორიის მასალები | Kr. Šarašiʒe, Samxret Sakartvelos isṭoriis masalebi (XV-XVI ss.), Tbilisi 1961, p. 15ff."
+        }
+    },
+    "entries": [
+        // --- Manuscript: Q-969 ---
+        {
+            "id": "q969_entry_1",
+            "n": 1, // The entry number
+            "manuscript": "Q-969",
+            "georgian": {
+                "text": "ბაგრატ აფხაზთა და ქართუჱლთა მართლმადიდებლისა მეფისა საუკუნოჲმცა არს საჴსენებელი.",
+                "markup": [{"type": "persName", "start": 0, "end": 6, "ref": "#p1001", "name": "ბაგრატ"}]
+            },
+            "english": {"text": "For Bagrat, orthodox king of the Abkhazians and Kartvelians, may his memory be eternal."},
+            "prosopography": [{"ref": "#p1001", "name": "ბაგრატ IV", "role": "main"}]
+        },
+        {
+            "id": "q969_entry_2",
+            "n": 2,
+            "manuscript": "Q-969",
+            "georgian": {
+                "text": "გიორგი კესაროსისა, აფხაზთაჲ, ქართველთა მეფისა ...",
+                "markup": [{"type": "persName", "start": 0, "end": 6, "ref": "#p1002", "name": "გიორგი"}]
+            },
+            "english": {"text": "Of Giorgi the Caesar, of the Abkhazians, King of the Kartvelians..."},
+            "prosopography": [{"ref": "#p1002", "name": "გიორგი II", "role": "main"}]
+        },
+        // ... All other 16 entries remain exactly the same as before ...
+        {
+            "id": "q969_entry_3", "n": 3, "manuscript": "Q-969",
+            "georgian": { "text": "დავით ყოვლისა აღმოსავლეთისა მეფისა, ახლისა მართლმადიდებელისა საუკუნოჲმცა საჴსენებელი და კურთხევა მისი.", "markup": [{"type": "persName", "start": 0, "end": 5, "ref": "#p1003", "name": "დავით"}] },
+            "english": {"text": "Of Davit, king of all the East, the new orthodox one, may his memory and blessing be eternal."},
+            "prosopography": [{"ref": "#p1003", "name": "დავით IV აღმაშენებელი", "role": "main"}]
+        },
+        {
+            "id": "q969_entry_4", "n": 4, "manuscript": "Q-969",
+            "georgian": { "text": "დემეტრე ყოვლისა აღმოსავლეთისა მეფისა საუკუნოჲმცა საჴსენებელი და კურთხევაჲ მისი.", "markup": [{"type": "persName", "start": 0, "end": 7, "ref": "#p1004", "name": "დემეტრე"}] },
+            "english": {"text": "Of Demetre, king of all the East, may his memory and blessing be eternal."},
+            "prosopography": [{"ref": "#p1004", "name": "დემეტრე I", "role": "main"}]
+        },
+        {
+            "id": "q969_entry_5", "n": 5, "manuscript": "Q-969",
+            "georgian": { "text": "გიორგი, ყოვლისა აღმოსავლეთისა ჴელმწიფედ მპყრობელისა, მეფეთ-მეფისა საუკუნოჲმცა არს საჴსენებელი და კურთხევაჲ მისი.", "markup": [{"type": "persName", "start": 0, "end": 6, "ref": "#p1005", "name": "გიორგი"}] },
+            "english": {"text": "Of Giorgi, ruler of all the East, King of Kings, may his memory and blessing be eternal."},
+            "prosopography": [{"ref": "#p1005", "name": "გიორგი III", "role": "main"}]
+        },
+        {
+            "id": "q969_entry_6", "n": 6, "manuscript": "Q-969",
+            "georgian": { "text": "თამარ ყოვლისა აღმოსავლეთისა ახლისა მართლმადიდებლისა, აფხაზთა, ქართველთაჲ, ჰერთა და კახთა და ყოველთა ქრისტეანეთა მპყრობელისა საუკუნოჲმცა არს საჴსენებელი და კურთხევაჲ მისი.", "markup": [{"type": "persName", "start": 0, "end": 5, "ref": "#p1006", "name": "თამარ"}] },
+            "english": {"text": "Of Tamar, new orthodox one of all the East, ruler of the Abkhazians, Kartvelians, Heretians, and Kakhetians, and all Christians, may her memory and blessing be eternal."},
+            "prosopography": [{"ref": "#p1006", "name": "თამარ მეფე", "role": "main"}]
+        },
+        {
+            "id": "q969_entry_7", "n": 7, "manuscript": "Q-969",
+            "georgian": { "text": "ძისა მათისა გიორგი ყოვლისა აღმოსავლეთისა და დასავლეთისა მტკიცედ მპყრობლისა მეფეთ-მეფისა მრავალმცა არიან წელნი მეფობისა მისისანი ...", "markup": [{"type": "persName", "start": 13, "end": 19, "ref": "#p1007", "name": "გიორგი"}] },
+            "english": {"text": "Of their son, Giorgi, firm ruler of all the East and West, King of Kings, may the years of his reign be many..."},
+            "prosopography": [{"ref": "#p1007", "name": "გიორგი IV ლაშა", "role": "kinship"}]
+        },
+        {
+            "id": "q969_entry_8", "n": 8, "manuscript": "Q-969",
+            "georgian": { "text": "დიდისა და წარჩინებულისა და სიმხნით განთქმულისა და სახელოვანისა და მტერთაგან უძლეველისა ათაბაგ-ამირსპალარისა, მორწმუნისა პატრონისა ჩუენისა ყუარყუარესი საუკუნომცა არს საჴსენებელი და კურთხევა მისი.", "markup": [{"type": "persName", "start": 128, "end": 140, "ref": "#p1008", "name": "ყუარყუარესი"}] },
+            "english": {"text": "Of our patron, the great, distinguished, renowned for bravery, glorious and invincible by enemies, Atabag-Amirspasalar, the believer Qvarqvare, may his memory and blessing be eternal."},
+            "prosopography": [{"ref": "#p1008", "name": "ყვარყვარე II ჯაყელი", "role": "main", "occupation": "ათაბაგი"}]
+        },
+        {
+            "id": "q969_entry_9", "n": 9, "manuscript": "Q-969",
+            "georgian": { "text": "ეგრეთვე მეუღლისა მათისა, დედათა დიდებისა და სიკეთისა ძეგლისა, წმიდათა მენელსაცხებლეთა მოდასისა და სარწმუნოებისა ბეჭედსა და კეთილად მოღუაწისა, ღმრთისმოყუარისა პატრონის დედისიმედის საუკუნოჲცა არს საჴსენებელი და კურთხევაჲ.", "markup": [{"type": "persName", "start": 166, "end": 178, "ref": "#p1009", "name": "დედისიმედის"}] },
+            "english": {"text": "Likewise, of his wife, the glory of women and monument of goodness, peer of the holy Myrrhbearers, seal of faith and good ascetic, the God-loving patron Dedisimedi, may her memory and blessing also be eternal."},
+            "prosopography": [{"ref": "#p1009", "name": "დედისიმედი", "role": "wife", "relationTo": "#p1008"}]
+        },
+        {
+            "id": "q969_entry_10", "n": 10, "manuscript": "Q-969",
+            "georgian": { "text": "და პირმშოსა ძისა მათისა, სიმჴნით ქებულისა და სიბრძნის გამომეძიებელისა და დაუცხრომელად ღმრთის მადიდებელისა პატრონის ქაიხოსროს საუკუნოჲმცა არს საჴსენებელი და კურთხევაჲ მისი.", "markup": [{"type": "persName", "start": 113, "end": 122, "ref": "#p1010", "name": "ქაიხოსროს"}] },
+            "english": {"text": "And of their firstborn son, praised for bravery, seeker of wisdom, and tireless glorifier of God, the patron Kaikhosro, may his memory and blessing be eternal."},
+            "prosopography": [{"ref": "#p1010", "name": "ქაიხოსრო I ჯაყელი", "role": "son", "relationTo": "#p1008"}]
+        },
+        {
+            "id": "q969_entry_11", "n": 11, "manuscript": "Q-969",
+            "georgian": { "text": "და შემდგომისა და საყუარელისა ძმისა მისისა, ღმრთივ განბრძნობილისა, მჴნედ მჴედრისა და მრავალთა და ძლიერთა და სახელდებულთა ფალავანთა დამამჴობელისა და ქედისა შემმუსრველისა, საქრისტიანოსა ზღუდისა, სრულისა ჩოხოსნობისა და ანგელოზებრივისა სქემისა ღირსად მიმღებელისა პატრონისა ჩუენისა მზეჭაბუკ-ყოფილსა იაკობისი საუკუნოჲმცა არს საჴსენებელი და კურთხევაჲ მისი.", "markup": [{"type": "persName", "start": 260, "end": 288, "ref": "#p1011", "name": "მზეჭაბუკ-ყოფილსა იაკობისი"}] },
+            "english": {"text": "And of his next and beloved brother, made wise by God, a valiant rider and overthrower and neck-crusher of many strong and famous champions, a fence for Christianity, worthy recipient of the full monastic habit and the angelic schema, our patron Mzechabuk, formerly Iakob, may his memory and blessing be eternal."},
+            "prosopography": [{"ref": "#p1011", "name": "მზეჭაბუკ I ათაბაგი", "role": "kinship", "relationTo": "#p1010"}]
+        },
+        {
+            "id": "q969_entry_12", "n": 12, "manuscript": "Q-969",
+            "georgian": { "text": "და ძმისწულისა მათისა, მჴნისა და ახოვანისა და ქველ-პირმეტყუჱლისა, აგარიანთა მომსრველისა, გულოვნებით სახელგანთქმულისა, მტერთა შემაძრწუნებელისა, სარწმუნოებისა და ქრისტეს ჭეშმარიტისა სჯულისა მტკიცედ მპყრობელისა და აწ მფლობელისა ჩუენისა პატრონისა ნავ (!) ყუარყუარესი საუკუნოჲმცაჲ არს საჴსენებელი და კურთხევაჲ მისი, მრავალმცა არიან დღენი და ჟამნი ცხორებისაჲ მისისანი.", "markup": [{"type": "persName", "start": 235, "end": 251, "ref": "#p1012", "name": "ნავ (!) ყუარყუარესი"}] },
+            "english": {"text": "And of their nephew, the valiant and heroic and eloquent, the destroyer of the Hagarenes, renowned for courage, the terror of enemies, firm holder of the faith and true law of Christ, and our present ruler, patron Nav (!) Qvarqvare, may his memory and blessing be eternal, may the days and times of his life be many."},
+            "prosopography": [{"ref": "#p1012", "name": "ყვარყვარე III ჯაყელი", "role": "kinship", "relationTo": "#p1011"}]
+        },
+        {
+            "id": "q969_entry_13", "n": 13, "manuscript": "Q-969",
+            "georgian": { "text": "და მეუღლისა მათისა, ღუთივ-გჳრგჳნოსანისა (!) მთიებისა და ცისკროვნებრ ბრწყინვალისა, სიმშჳდის მეძიებელისა და წერილთ-მოყუარულისა და ჯუართა და ხატთა პირად-პირადად მამკობელისა პატრონისა ჩუენისა ანნაჲსი მრავალმცა არიან დღენი და ჟამნი ცხორებისა მისისასანი.", "markup": [{"type": "persName", "start": 190, "end": 197, "ref": "#p1013", "name": "ანნაჲსი"}] },
+            "english": {"text": "And of his wife, the divinely-crowned luminary and brilliant like the morning star, seeker of peace and lover of letters and decorator of various crosses and icons, our patron Anna, may the days and times of her life be many."},
+            "prosopography": [{"ref": "#p1013", "name": "ანნა", "role": "wife", "relationTo": "#p1012"}]
+        },
+        {
+            "id": "q969_entry_14", "n": 14, "manuscript": "Q-969",
+            "georgian": { "text": "და ძეთა მათთა, სამოთხის ნერგთა, ვარდთა სურნელთა და გონიერთა, საღმრთოდ-სამჴედროდ აღზრდილთა პატრონის მზეჭაბუკისი და პატრონის ქაიხოსროსი მრავალმცა არიან დღენი და ჟამნი ცხორებისა მათისანი.", "markup": [{"type": "persName", "start": 105, "end": 116, "ref": "#p1014", "name": "მზეჭაბუკისი"}, {"type": "persName", "start": 131, "end": 141, "ref": "#p1015", "name": "ქაიხოსროსი"}] },
+            "english": {"text": "And of their sons, saplings of paradise, fragrant roses and intelligent ones, raised for divine military service, the patron Mzechabuk and the patron Kaikhosro, may the days and times of their lives be many."},
+            "prosopography": [{"ref": "#p1014", "name": "მზეჭაბუკ ჯაყელი", "role": "son", "relationTo": "#p1012"}, {"ref": "#p1015", "name": "ქაიხოსრო II ჯაყელი", "role": "son", "relationTo": "#p1012"}]
+        },
+        {
+            "id": "q969_entry_15", "n": 15, "manuscript": "Q-969",
+            "georgian": { "text": "და სოილ კოპას ძისა, ტავოსა ამილახორისა და მხატვრობით კეთილად ჴელოვნისა და სიბრძნის-მეტყუჱლისა და საღმრთოთა წერილთა მართლიად აღმასრულებელია და თარგმანთა განმამარტებელისა მრავალმცა არიან დღენი და ჟამნი სუფევისასა მისისანი და კუალადცა საუკუნომცა არს საჴსენებელი და კურთხევაჲ მისი.", "markup": [{"type": "persName", "start": 3, "end": 20, "ref": "#p1016", "name": "სოილ კოპას ძისა"}] },
+            "english": {"text": "And of Soil, son of Kopa, the Tavos Amilakhori and well-skilled in painting and a speaker of wisdom and a righteous executor of divine writings and an explainer of translations, may the days and times of his reign be many, and again, may his memory and blessing be eternal."},
+            "prosopography": [{"ref": "#p1016", "name": "სოილ", "patronymic": "კოპას ძე", "role": "main", "occupation": "ამილახორი, მხატვარი"}]
+        },
+        {
+            "id": "q969_entry_16", "n": 16, "manuscript": "Q-969",
+            "georgian": { "text": "ეგრეთვე მართლმადიდებელთა და კურთხეულთა მშობელთა მისთა რუამ-ყოფილისა რაშთაელისი და რაინდო-ყოფილისა ეკატირინესი საუკუნოჲმცა არს საჴსენებელი და კურთხევაჲ მათი.", "markup": [{"type": "persName", "start": 57, "end": 82, "ref": "#p1017", "name": "რუამ-ყოფილისა რაშთაელისი"}, {"type": "persName", "start": 86, "end": 111, "ref": "#p1018", "name": "რაინდო-ყოფილისა ეკატირინესი"}] },
+            "english": {"text": "Likewise, of his orthodox and blessed parents, Ruam-yofili Rashtaeli and Raindo-yofili Ekaterine, may their memory and blessing be eternal."},
+            "prosopography": [{"ref": "#p1017", "name": "რუამ-ყოფილი რაშთაელი", "patronymic": "კოპას ძე", "role": "parent", "relationTo": "#p1016"}, {"ref": "#p1018", "name": "რაინდო-ყოფილი ეკატირინე", "role": "parent", "relationTo": "#p1016"}]
+        },
+        {
+            "id": "q969_entry_17", "n": 17, "manuscript": "Q-969",
+            "georgian": { "text": "და მემცხედრისა მისისა, ღუაწლ-მრავალ და მოღუაწებით წმიდათა ქალწულთა მიმსგავსებულისა, ლოცვა-მარხვით დაუშრომელსა და სრულსა მონაზონებისა და სქემოსნობისა ღირსებით სრულქმნილსა, მანგუშარ-ყოფილსა მართასი საუკუნოჲმცა არს საჴსენებელი და კურთხევაჲ მისი.", "markup": [{"type": "persName", "start": 169, "end": 194, "ref": "#p1019", "name": "მანგუშარ-ყოფილსა მართასი"}] },
+            "english": {"text": "And of his female relative, of many labors and assimilated to the holy virgins through asceticism, tireless in prayer and fasting and perfected with the dignity of full monasticism and the schema, Mangushar-yofili Marta, may her memory and blessing be eternal."},
+            "prosopography": [{"ref": "#p1019", "name": "მანგუშარ-ყოფილი მართა", "role": "kinship", "relationTo": "#p1016"}]
+        },
+        {
+            "id": "q969_entry_18", "n": 18, "manuscript": "Q-969",
+            "georgian": { "text": "ძეთა და ასულთა მათთა მიცვალებულთა საუკუნოჲმცა არს საჴსენებელი და კურთხევა მათი ... და ცოცხალთა მრავალმცა არიან ჟამნი და დღენი ცხორებისაჲ მათისანი, ამინ.", "markup": [] },
+            "english": {"text": "Of their deceased sons and daughters, may their memory and blessing be eternal... and for the living, may the times and times of their lives be many, Amen."},
+            "prosopography": []
+        }
+    ]
+};
